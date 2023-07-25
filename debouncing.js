9d541ef143fa1ca821b331d2265d88400b8c5bf5
sachin shakya <sachinshakya205001@gmail.com>
2023-07-25 10:53:03 +0530

    feat : debouncing

--- debouncing.js
+++ debouncing.js
@@ -1,3 +1,20 @@
-const handleChange = () => {
-  console.log("working");
+/// Debouncing - it is technique where we delay the execution of a function until
+///              after a certain amount of time has passed.
+
+const getData = () => {
+  console.log("fetching data");
 };
+
+const debounce = (callback, d) => {
+  let timer;
+  return function () {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      callback();
+    }, d);
+  };
+};
+
+const betterFunction = debounce(getData, 2000);
+
+document.getElementById("btn").addEventListener("click", betterFunction);
